9f21b6d13af0b6faccbb0d8bc08a280993b9f84f
mahboob <mahboob.eid@googlemail.com>
2018-04-15 08:15:56 +0200

    modified the order.

--- Specifications/Framework/Specification.cs
+++ Specifications/Framework/Specification.cs
@@ -8,21 +8,21 @@ namespace Specifications
 
         public abstract bool IsSatisfied(T candidate);
 
-
         public ISpecification<T> And(ISpecification<T> entity)
         {
             return new AndSpecification<T>(this, entity);
-        }
-
-       
-        public ISpecification<T> Not()
-        {
-            return new NotSpecification<T>(this);
         }
 
         public ISpecification<T> Or(ISpecification<T> entity)
         {
             return new OrSpecification<T>(this, entity);
         }
+
+        public ISpecification<T> Not()
+        {
+            return new NotSpecification<T>(this);
+        }
+
+       
     }
 }
